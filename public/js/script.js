0fbc7fb15ef45e9f1c2d2d2524f83707bb2170ce
vinicius <vini26925@gmail.com>
2023-12-01 00:55:14 -0300

    deixando a mudança de tema dinamica

---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+function alterartema(){
+    const tema = localStorage.getItem("tema")
+    const bory = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema){
+        let novoTema
+
+        if (tema == "Light"){
+            novoTema = "dark"
+            button.innerHTML= `<img src="/imagens/sun-icon.png" alt="icone da sol>`
+            bory.classList.remove("light")
+            bory.classList.add("dark")
+        }else {
+            novoTema = "Light"
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="icone da lua">`
+            bory.classList.remove("dark")
+            bory.classList.add("light")
+        }
+
+
+        localStorage.setItem("tema", novoTema)
+        return
+    }
+
+    localStorage.setItem("tema", "dark")
+    bory.classList.add("dark")
+}
+
+function verificartema(){
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+        if (tema) {
+            if (tema == "dark"){
+                body.classList.add("dark")
+                button.innerHTML= `<img src="/imagens/sun-icon.png" alt="icone da sol>`
+            }else {
+                body.classList.add("light")
+                button.innerHTML = `<img src="/imagens/moon-icon.png" alt="icone da lua">`
+            }
+        }
+    
+}
+
+verificartema()
